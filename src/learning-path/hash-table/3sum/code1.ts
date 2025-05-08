d7c02ecc4df55eec85cb3b2c19f65038f1a04478
Li Qile <qile.li@shopee.com>
2025-05-08 18:11:26 +0800

    提交

---
+++ src/learning-path/hash-table/3sum/code1.ts
@@ -0,0 +1,26 @@
+function threeSum(nums: number[]): number[][] {
+  const countMap: Map<number, number> = new Map();
+  nums = nums.filter((val) => {
+    if (countMap.has(val)) {
+      const count = countMap.get(val)!;
+      countMap.set(val, count + 1);
+    } else {
+      countMap.set(val, 1);
+    }
+
+    return countMap.get(val)! <= 3;
+  });
+  const setArr = nums.map((_, index) => new Set(nums.slice(index)));
+  const resSet: Set<string> = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      const sum = -nums[i] - nums[j];
+      if (setArr[j + 1].has(sum)) {
+        const resArr = [sum, nums[i], nums[j]].sort((a, b) => a - b);
+        resSet.add(JSON.stringify(resArr));
+      }
+    }
+  }
+
+  return Array.from(resSet).map((str) => JSON.parse(str));
+}
---
+++ src/learning-path/hash-table/3sum/code2.ts
@@ -0,0 +1,6 @@
+function threeSum(nums: number[]): number[][] {
+  const res: number[][] = [];
+  const sortedNums = nums.sort((a, b) => a - b);
+
+  return res;
+}
