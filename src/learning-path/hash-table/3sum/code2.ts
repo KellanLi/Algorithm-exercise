10dd8d8c284ee00a698a9be3ec6b6d38226fcc20
Li Qile <qile.li@shopee.com>
2025-05-14 18:44:56 +0800

    三数之和双指针

--- src/learning-path/hash-table/3sum/code2.ts
+++ src/learning-path/hash-table/3sum/code2.ts
@@ -1,6 +1,28 @@
 function threeSum(nums: number[]): number[][] {
   const res: number[][] = [];
   const sortedNums = nums.sort((a, b) => a - b);
-
+  let lastVal: number | undefined;
+  for (let i = 0; i < sortedNums.length - 2; i++) {
+    if (sortedNums[i] === lastVal) continue;
+    let left = i + 1;
+    let right = sortedNums.length - 1;
+    let lastLeftVal: number | undefined, lastRightVal: number | undefined;
+    while (left < right) {
+      const sum = sortedNums[i] + sortedNums[left] + sortedNums[right];
+      if (sum === 0) {
+        if (
+          lastLeftVal !== sortedNums[left] &&
+          lastRightVal !== sortedNums[right]
+        )
+          res.push([sortedNums[i], sortedNums[left], sortedNums[right]]);
+        lastLeftVal = sortedNums[left];
+        lastRightVal = sortedNums[right];
+        left++;
+      }
+      if (sum < 0) left++;
+      if (sum > 0) right--;
+    }
+    lastVal = sortedNums[i];
+  }
   return res;
 }
